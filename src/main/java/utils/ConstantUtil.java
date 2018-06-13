789fa9612945ecfdad5a53e3582b3e08b65a1d96
fanfan <qiyetingxue24@163.com>
2018-06-13 17:51:47 +0800

    this is 1th commit

---
+++ src/main/java/utils/ConstantUtil.java
@@ -0,0 +1,8 @@
+package utils;
+
+/**
+ * Created by li on 2018/6/13 0013.
+ */
+public class ConstantUtil {
+    public static final int CODE=1;
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! This is third time I have configured IDEA</h2>
+</body>
+</html>
