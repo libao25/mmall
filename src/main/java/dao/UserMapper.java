a55e5228286fb5e7dec04790e88e3255aaaf8f70
fanfan <qiyetingxue24@163.com>
2018-06-14 16:20:31 +0800

    send to github once again

--- src/main/java/dao/UserMapper.java
+++ src/main/java/dao/UserMapper.java
@@ -4,4 +4,9 @@ package dao;
  * Created by Administrator on 2018/6/14 0014.
  */
 public class UserMapper {
+    //这就是测试一下对github的远程提交
+    public static final int CODE=0;
+    public static final int CODE1=1;
+
+
 }
